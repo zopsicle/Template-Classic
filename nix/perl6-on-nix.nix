d313385fa38e88f4b7b0d044b32ab1b2b4cfb5c9
Chloé <rightfold+ck@gmail.com>
2019-08-04 21:58:49 +0200

    Add tests

--- nix/perl6-on-nix.nix
+++ nix/perl6-on-nix.nix
@@ -1,7 +1,7 @@
 let
     tarball = fetchTarball {
-        url = "https://github.com/chloekek/cp6t/archive/44822968e895abcfeb5309d30bfc30c105b8fdee.tar.gz";
-        sha256 = "08a24m22ndz3ck4lcg8k2azkx5pr7n66qf9k7l63s471dc07cmmj";
+        url = "https://github.com/chloekek/cp6t/archive/63975f226039f996941d99ecdb079ef92c8ff820.tar.gz";
+        sha256 = "0a0pbdajqx1wp55jfi6khy6wflhspmhf07yqakn1rqhhjhgffhq7";
     };
 in
     import (tarball + "/perl6-on-nix")
---
+++ t/test.t
@@ -0,0 +1,11 @@
+use Test;
+use Template::Classic;
+
+my $src := ｢<ul><% for @xs -> $x { %><li><%= $x %></li><% } %></ul>｣;
+my &t := *.eager.join ∘ template(:(@xs), $src);
+
+cmp-ok t(qw｢        ｣), ‘eq’, ｢<ul></ul>｣;
+cmp-ok t(qw｢foo bar ｣), ‘eq’, ｢<ul><li>foo</li><li>bar</li></ul>｣;
+cmp-ok t(qw｢foo <&"'｣), ‘eq’, ｢<ul><li>foo</li><li>&lt;&amp;&quot;&#39;</li></ul>｣;
+
+done-testing;
